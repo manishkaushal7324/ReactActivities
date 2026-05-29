ef4a4f02917809cd2abaa9b1193eb95f3c0ef6b2
Manish.Kaushal <Manish.Kaushal@BNGECO-L-79721>
2026-05-29 13:05:56 +0530

    Section_8

--- Application/obj/Debug/net10.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net10.0/Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15597e1aeb98916c837bb6fb4714ac015b2e45b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+488047550283e36eade73f75ca70584be7a6e4ff")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
